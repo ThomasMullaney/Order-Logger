a6543f8c8fffecdfc12351bbf11ad03d515f2ff5
Thomas Mullaney <tloring.mullaney@gmail.com>
2020-08-29 16:13:20 -0700

    Post function works

---
+++ orderApp/Solved/db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Meeses');
+INSERT INT burgers (name) VALUES ('Bobbi');
+INSERT INT burgers (name, devour) VALUES ('Sylvester', true);
+INSERT INT burgers (name, devour) VALUES ('Marilyn Manson', true);
+INSERT INT burgers (name, devour) VALUES ('Joe Biden', true);
+INSERT INT burgers (name) VALUES ('Owens');
